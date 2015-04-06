1836ec327c27bb9b08bc995b7823b2df8f751eb7
Tomáš Doležal <tomas.dolezal@gmail.com>
2015-04-06 21:38:13 +0200

    Fix build

--- build.cmd
+++ build.cmd
@@ -10,9 +10,9 @@ IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
 :copynuget
-IF EXIST packages\nuget.exe goto restore
-md packages
-copy %CACHED_NUGET% packages\nuget.exe > nul
+IF EXIST .nuget\nuget.exe goto restore
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
 IF EXIST packages\Sake goto run
