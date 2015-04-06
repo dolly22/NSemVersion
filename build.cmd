589ec9d422cdff646f92e79b615a6e4d32ee901e
Tomáš Doležal <tomas.dolezal@gmail.com>
2015-04-06 08:34:16 +0200

    Appveyor

--- build.cmd
+++ build.cmd
@@ -7,7 +7,8 @@ SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
 IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+echo Created nuget cache folder
+@powershell -NoProfile -ExecutionPolicy unrestricted -Scope Process -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
 :copynuget
 IF EXIST packages\nuget.exe goto restore
@@ -16,7 +17,9 @@ copy %CACHED_NUGET% packages\nuget.exe > nul
 
 :restore
 IF EXIST packages\Cake goto run
+echo Installing cake build
 packages\NuGet.exe install Cake -version 0.2.2 -o packages -ExcludeVersion
 
 :run
+echo Starting cake
 packages\Cake\Cake.exe build.cake %*
